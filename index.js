0c99215e53f6837b51e5e6f2ba2cbed19566bb55
James Halliday <mail@substack.net>
2013-04-20 17:09:21 -0700

    stubbed out tie code to set timestamp watermarks

--- index.js
+++ index.js
@@ -25,9 +25,37 @@ Logdir.prototype.createWriteStream = function (name) {
     return tr;
 };
 
-Logdir.prototype.open = function (name, opts) {
-    return sf(path.join(this.dir, name));
+Logdir.prototype.open = function (names, opts) {
+    var self = this;
+    if (Array.isArray(names)) {
+        var files = names.reduce(function (acc, name) {
+            acc[name] = self.open(name, opts);
+            return acc;
+        }, {});
+        return {
+            slice: tie('slice', files),
+            follow: tie('follow', files)
+        }
+    }
+    else {
+        return sf(path.join(this.dir, names));
+    }
 };
+
+function tie (method, files) {
+    return function () {
+        var args = arguments;
+        var tr = through();
+        
+        Object.keys(files).forEach(function (key) {
+            var s = files[key][method].apply(files[key], args);
+            s.pipe(through(function (line) {
+                console.dir(line);
+            }));
+        });
+        return tr;
+    };
+}
 
 Logdir.prototype.list = function (cb) {
     fs.readdir(this.dir, cb);
